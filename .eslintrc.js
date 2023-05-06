c85ed2e7bfe59736a9e47b366ccfdd698b5fb78b
Enrique Marin <enrique_marin_h@hotmail.com>
2023-05-06 14:07:03 -0700

    feat: add Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'indent': [ 'error', 2 ],
+    'eqeqeq': ['error', 'smart'],
+    'linebreak-style': [ 'error', 'unix' ],
+    'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'semi': [ 'error', 'never' ],
+    'no-console': 'off',
+    'no-debugger': 'warn',
+    'no-var': 'error',
+    'no-trailing-spaces': 'error',
+    'space-before-function-paren': ['error', 'never'],
+    'comma-dangle': ['error', {
+      objects: 'always-multiline',
+      arrays: 'always-multiline',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    }],
+  },
+  'globals': {
+    window: 'readonly',
+  },
+  'settings': {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+}
